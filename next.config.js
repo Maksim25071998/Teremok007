78bac3ce8b40f5082f29b3bb50719beac9467bfb
Maksim25071998 <sacyilomaksim@gmail.com>
2025-07-25 07:30:39 +0300

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  i18n: {
+    locales: ['ua', 'en'],
+    defaultLocale: 'ua',
+  },
+};
+
+module.exports = nextConfig;
